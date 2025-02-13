f3b913e5d57765504ea125f39b61861e644a6937
1MAGNOVA <olalekanabsm@gmail.com>
2025-02-12 17:39:32 -0800

    sql

---
+++ MySQLServer.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        if 
+
+
+        my_db = mysql.connector.connect(
+            host='localhost',
+            user='your_username',   
+            passwordd='your_password'
+
+            )
+
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            # Create database if it doesn't exist
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+            print("Database 'alx_book_store' created successfully!")
+    
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+    
+    finally:
+        # Close cursor and connection
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+
+if __name__ == "__main__":
+    create_database()
